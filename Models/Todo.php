4a97dac36cb5c3058c1aefbdc90dac1a58c23ebc
枳本望 <nozomusp29@gmail.com>
2019-10-29 20:58:27 +0800

    Todoリスト
    復習

---
+++ Models/Todo.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once ('config/dbconnect.php');
+// require_once 'config/dbconnect.php';
+
+class Todo
+{
+    private $table = 'tasks';
+    private $db_manager;
+
+    public function __construct()
+    {
+        $this->db_manager = new DbManager();
+        $this->db_manager->connect();
+    }
+    public function create($name)
+    {
+        $stmt = $this->db_manager->dbh->prepare('INSERT INTO '.$this->table.' (name) VALUES (?)');
+        $stmt->execute([$name]);
+    }
+    public function all()
+    {
+        $stmt = $this->db_manager->dbh->prepare('SELECT * FROM '.$this->table);
+        $stmt->execute();
+        $tasks = $stmt->fetchAll();
+
+        return $tasks;
+    }
+}
